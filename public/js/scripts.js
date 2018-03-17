5829bebd537dac142bc56dbe0074e25b54ed38f5
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-16 22:53:54 -0600

    Generatepalette of 5 random colors on page load

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,12 +3,27 @@ $('#generate-colors-btn').on('click', () => generatePalette());
 
 const generatePalette = () => {
   console.log('generate palette was called')
-  const hex = genRandomHex();
-  console.log(hex)
+  const palette = colorPalette();
+  console.log(palette)
 }
 
-const genRandomHex = () => {
+const generateRandomHex = () => {
   const hex = '#'+Math.random().toString(16).slice(-6)
   return hex
+}
+
+const colorPalette = () => {
+  const palette = [];
+
+  for(let i = 0; i < 5; i++) {
+    let swatch = generateRandomHex()
+
+    palette.push({
+      swatch: swatch
+    })
+  }
+
+  console.log(palette);
+  // let paletteIds = ['swatch-1', 'swatch-2', 'swatch-3', 'swatch-4', 'swatch-5' ]
 }
 
