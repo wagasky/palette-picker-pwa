052ffccbdfb6ee905ed94d21f86eeeae7ddc676e
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-16 22:11:56 -0600

    Set up procfile for future heroku deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+const $ = require("jquery");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(express.static('public'))
+// app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = "Palette Picker";
+app.use(bodyParser.json());
+
+app.get('/', (request, response) => {
+
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+})
