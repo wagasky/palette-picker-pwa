0f94db5eecbd3cd6ad3e455308cad3410f44e929
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-16 23:32:52 -0600

    Render color swatches to palette container

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,7 +2,7 @@ $(window).on('load', () => generatePalette());
 $('#generate-colors-btn').on('click', () => generatePalette());
 
 const generatePalette = () => {
-  const palette = colorPalette();
+  renderPalette();
 }
 
 const generateRandomHex = () => {
@@ -10,20 +10,39 @@ const generateRandomHex = () => {
   return hex
 }
 
-const colorPalette = () => {
+const renderPalette = () => {
+  const newPalette = createPalette();
+
+  newPalette.map(color => renderSwatch(color))
+  return newPalette;
+}
+
+const createPalette = () => {
   const palette = [];
 
   for(let i = 0; i < 5; i++) {
     let swatch = generateRandomHex()
 
-    palette.push({
-      swatch: swatch
-    })
+    palette.push({ swatch })
   }
 
   putIntoStorage(palette);
+  return palette;
   // let paletteIds = ['swatch-1', 'swatch-2', 'swatch-3', 'swatch-4', 'swatch-5' ]
+} 
+
+const renderSwatch = (color) => {
+  const swatchSection = document.querySelector(".color-swatch--container")
+  const swatchArticle = document.createElement("article");
+  swatchArticle.setAttribute("class", "color-swatch");
+  swatchArticle.setAttribute("style", `background-color: ${color.swatch}`);
+
+  swatchArticle.innerHTML = `<p>${color.swatch}</p>`
+
+  swatchSection.appendChild(swatchArticle);
 }
+
+
 
 const putIntoStorage = (palette) => {
   localStorage.setItem("palette", JSON.stringify(palette))
@@ -33,8 +52,3 @@ const pullFromStorage = () => {
   return JSON.parse(localStorage.getItem("palette"))
 }
 
-
-// const renderPalette
-// call generate palette
-// append the colors to the HTML elements
-// 
