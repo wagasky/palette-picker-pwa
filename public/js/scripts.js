5850afab46ec8e841d6b0aba3febb38285f5fca4
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-17 18:26:49 -0600

    Add color lock button and state toggle

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -22,9 +22,10 @@ const createPalette = () => {
   const palette = [];
 
   for(let i = 0; i < 5; i++) {
-    let swatch = generateRandomHex()
+    let swatch = generateRandomHex();
+    let locked = false;
 
-    palette.push({ swatch })
+    palette.push({ swatch, locked })
   }
 
   putIntoStorage(palette);
@@ -32,26 +33,29 @@ const createPalette = () => {
   // let paletteIds = ['swatch-1', 'swatch-2', 'swatch-3', 'swatch-4', 'swatch-5' ]
 } 
 
-const renderSwatch = (color) => {
+const renderSwatch = (swatch) => {
   const swatchSection = document.querySelector(".color-swatch--container")
   const swatchArticle = document.createElement("article");
+  const lockClass = swatch.locked ? "fas fa-lock" : "fas fa-lock-open";
   swatchArticle.setAttribute("class", "color-swatch");
-  swatchArticle.setAttribute("style", `background-color: ${color.swatch}`);
+  swatchArticle.setAttribute("style", `background-color: ${swatch.swatch}`);
 
-  swatchArticle.innerHTML = `<p>${color.swatch}</p>`
-
+  swatchArticle.innerHTML = `
+    <p>${swatch.swatch}</p>
+    <button type="button" class="${lockClass}"></button>
+  `
   swatchSection.appendChild(swatchArticle);
 }
 
 const clearPalette = () => {
   const swatchSection = document.querySelector(".color-swatch--container")
   while(swatchSection.hasChildNodes()) {
-    swatchSection.removeChild(swatchSection.lastChild)
+    swatchSection.removeChild(swatchSection.lastChild);
   }
 }
 
 const putIntoStorage = (palette) => {
-  localStorage.setItem("palette", JSON.stringify(palette))
+  localStorage.setItem("palette", JSON.stringify(palette));
 }
 
 const pullFromStorage = () => {
