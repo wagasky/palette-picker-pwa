ca6b712b6601ee7a38172e3edc7d6c23ac3e0bf5
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-22 18:07:46 -0600

    Test client route for homepage

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,7 +6,17 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-
+   it('should return the homepage', () => {
+    return chai.request(server)
+    .get('/')
+    .then(response => {
+      response.should.have.status(200);
+      response.should.be.html;
+    })
+    .catch(err => {
+      throw err;
+    });
+  });
 });
 
 describe('API Routes', () => {
