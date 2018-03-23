fd373fcb3c32bbbacab17d57c92f65acc6dbfaf2
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-22 22:05:21 -0600

    Add test for project api endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -32,4 +32,22 @@ describe('Client Routes', () => {
 
 describe('API Routes', () => {
 
+  describe('GET /api/v1/projects', () => {
+    it('should return all of the projects', () => {
+      return chai.request(server)
+      .get('/api/v1/projects')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('name')
+        response.body[0].name.should.equal('Bright Project')
+      })
+      .catch(err => {
+        throw err;
+      });
+    });
+  });
+
 });
